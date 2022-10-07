4d1e816ab804d8870cb2746301f7204a3bddc472
hilmysekali <zulhilmidharma@gmail.com>
2022-10-07 11:46:02 +0700

    menghapus locale/bahasa

--- src/layout/CustomAppBar.js
+++ src/layout/CustomAppBar.js
@@ -1,12 +1,12 @@
-import { LocalesMenuButton, AppBar } from 'react-admin';
+import { AppBar } from 'react-admin';
 import { Typography } from '@mui/material';
 
 export const CustomAppBar = (props) => (
     <AppBar {...props}>
         <Typography flex="1" variant="h6" id="react-admin-title"></Typography>
-        <LocalesMenuButton languages={[
+        {/* <LocalesMenuButton languages={[
             { locale: 'en', name: 'English' },
             { locale: 'id', name: 'Indonesia' },
-        ]} />
+        ]} /> */}
     </AppBar>
 );
