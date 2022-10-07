aba06211cac1e2266ddc84389c9b435babbe5173
hilmysekali <zulhilmidharma@gmail.com>
2022-10-07 14:34:16 +0700

    fix search and add filter kategori

--- src/components/products/ProductList.js
+++ src/components/products/ProductList.js
@@ -1,10 +1,17 @@
 import { useMediaQuery } from '@mui/material';
-import { List, SimpleList, Datagrid, TextField, EditButton, Filter, SearchInput, NumberField, BooleanField, useRecordContext } from 'react-admin';
-import { FirebaseReferenceField } from "../../FirebaseReferenceFields";
+import { List, SimpleList, Datagrid, TextField, EditButton, Filter, NumberField, BooleanField, useRecordContext, SelectInput, TextInput } from 'react-admin';
+import { FirebaseReferenceField, FirebaseReferenceInput } from "../../FirebaseReferenceFields";
 
 const ProductFilter = (props) => (
-    <Filter {...props}>
-        <SearchInput placeholder="Customer Email" source='nama' resettable alwaysOn />
+    <Filter {...props} disableSaveQuery>
+        <TextInput label="Nama Barang" source='nama' resettable alwaysOn />
+        <FirebaseReferenceInput
+            source="kategori_ref"
+            reference="categories"
+            alwaysOn
+        >
+            <SelectInput label="Kategori" optionText={"nama"} emptyText="Semua Kategori" fullWidth />
+        </FirebaseReferenceInput>
     </Filter>
 );
 
@@ -12,14 +19,14 @@ const CustomColorField = ({ source }) => {
     const record = useRecordContext();
     return record ? (
         <div>
-            <span 
-            style={{
-                height: '25px',
-                width: '25px',
-                backgroundColor: record[source],
-                borderRadius: '50%',
-                display: 'inline-block',
-            }}></span>
+            <span
+                style={{
+                    height: '25px',
+                    width: '25px',
+                    backgroundColor: record[source],
+                    borderRadius: '50%',
+                    display: 'inline-block',
+                }}></span>
         </div>
     ) : null;
 }
@@ -27,11 +34,11 @@ const CustomColorField = ({ source }) => {
 const ProductList = (props) => {
     const isSmall = useMediaQuery(theme => theme.breakpoints.down('sm'));
     return (
-        <List {...props} filters={<ProductFilter />} title='Product List'>
+        <List {...props} filters={<ProductFilter />} title='Product List' >
             {isSmall ? (
                 <SimpleList
                     primaryText={<TextField source="nama" />}
-                    secondaryText={record=>`${Intl.NumberFormat("id-ID", { style: "currency", currency: "IDR" }).format(record.harga)}`}
+                    secondaryText={record => `${Intl.NumberFormat("id-ID", { style: "currency", currency: "IDR" }).format(record.harga)}`}
                     rightAvatar={record => record.gambar[0].src}
                     linkType="edit"
                 />
@@ -42,10 +49,10 @@ const ProductList = (props) => {
                             <TextField source="nama" />
                         </FirebaseReferenceField>
                         <TextField source="nama" />
-                        <NumberField source="harga" locales={"id-ID"} options={{ style: 'currency', currency: 'IDR' }}/>
+                        <NumberField source="harga" locales={"id-ID"} options={{ style: 'currency', currency: 'IDR' }} />
                         <NumberField source="diskon" />
                         <TextField source="kondisi" />
-                        <CustomColorField source="warna_catatan" label='Warna'/>
+                        <CustomColorField source="warna_catatan" label='Warna' />
                         <BooleanField source="isActive" label="Active" />
                         <EditButton />
                     </Datagrid>
